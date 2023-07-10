4f18c5a710f7b76cdc79c13bbdeae329ea9ebb54
bob821229 <blue821229@gmail.com>
2023-07-10 15:30:40 +0800

    修改shopview

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,43 +1,28 @@
 <template>
-<<<<<<< HEAD
-  <h1>This is an shop page</h1>
-  <!-- <script>
-export default {
-  data() {
-    return {}
-  },
-  methods: {}
-}
-</script>
-<div class="wrap_main_dish">
-=======
     <div class="wrap_main_dish">
->>>>>>> 10649441abd384f0f8e39352c6a3754c95677c79
-        <div class="card" v-for="(item,index) in productList" :key="index">
-            <div class="pic">
-                <img :src="item.img">
+        <div class="wrap_main_dish">
+            <div class="card" v-for="(item, index) in productList" :key="index">
+                <div class="pic">
+                    <img :src="item.img" />
+                </div>
+                <h3>{{ item.name }}</h3>
+                <div class="btn_scd_s">選購</div>
             </div>
-            <h3>{{ item.name }}</h3>
-            <div class="btn_scd_s">選購</div>
         </div>
-    </div> -->
+    </div>
 </template>
-<<<<<<< HEAD
-<!-- <script>
-=======
 <script>
-import productList from '@/assets/data/productList.js'
->>>>>>> 10649441abd384f0f8e39352c6a3754c95677c79
+import productList from "@/assets/data/productList.js";
 export default {
-data() {
+    data() {
         return {
-                productList,
-        }
-},
-methods: {}
-}
+            productList,
+        };
+    },
+    methods: {},
+};
 </script>
 <style lang="scss">
 @import "@/assets/scss/all.scss";
 @import "@/assets/scss/page/shop.scss";
-</style>  -->
+</style>
