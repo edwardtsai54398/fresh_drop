1b89cdb8ffd2eedb40868ad55fc98795932ab0ac
lannysyu <oopsjohn.77@gmail.com>
2023-07-10 14:28:20 +0800

    新增產品介紹內容

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1>This is an product page</h1>
   <main>
     <div class="pro_header">
       <button class="btn_s btn_left">
@@ -12,6 +11,15 @@
         </div>
         {{ product[0].name }}
       </div>
+    </div>
+    <div class="pro_info" style="width:70%">
+      <div class="pic" style="height:300px">
+        <img src="../assets/images/product/10.jpg" alt="product">
+      </div>
+      <div class="text">
+        <p>泰式綠咖哩是一道充滿活力的料理，融合了辣味、椰奶和豐富的蔬菜，每一口都散發出獨特的香料風味，讓你彷彿置身於泰國的街頭小吃攤位。讓這道菜成為你生活中的一部分，帶給你愉悅的味覺體驗！</p>
+      </div>
+
     </div>
   </main>
 </template>
--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,12 +1,11 @@
 <template>
         <h1>This is an shop page</h1>
-
 </template>
 <script>
 export default {
         data() {
                 return {}
-            },
-            methods: {}
+        },
+        methods: {}
 }
 </script>
