6fbbed3a394479118e8077c9a17cfb7a58bbdbc9
dailinco <daylinc01229@gmail.com>
2023-07-10 17:10:26 +0800

    程式得救了

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -13,7 +12,7 @@
 export default {
   name: 'HomeView',
   components: {
-    //   HelloWorld
+  //   HelloWorld
   }
 }
 </script>
