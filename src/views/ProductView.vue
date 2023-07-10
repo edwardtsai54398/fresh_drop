247863275bf6c7754c0720a35853ce1d34c2a785
edwardtsai54398 <fashion.donk@gmail.com>
2023-07-10 22:05:59 +0800

    product頁面最層包 container 並以其做 scss樣式設定

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,95 +1,75 @@
 <template>
-  <section>
-    <div class="pro_header">
-      <button class="btn_s btn_left">
-        <span>back</span>
-        <i class="fa-sharp fa-solid fa-arrow-left"></i>
-      </button>
-      <div class="title">
-        <div class="type">
-          <span>{{ newProduct.catagory }}</span>
-        </div>
-        {{ newProduct.name }}
-      </div>
-    </div>
-    <div class="pro_info">
-      <div class="pic">
-        <img :src="newProduct.img" alt="product">
-      </div>
-      <div class="text">
-        <p>{{ productList[0].present }}</p>
-      </div>
-    </div>
-    <article>
-      <div>
-        <h2>材料【2人份】</h2>
-        <div class="wrap">
-          <p class="ingred" v-for="(item, index) in productList[0].ingred" :key="index">{{ item }}</p>
+  <div class="container product_container">
+    <section>
+      <div class="pro_header">
+        <button class="btn_s btn_left">
+          <span>back</span>
+          <i class="fa-sharp fa-solid fa-arrow-left"></i>
+        </button>
+        <div class="title">
+          <div class="type">
+            <span>{{ newProduct.catagory }}</span>
+          </div>
+          {{ newProduct.name }}
         </div>
       </div>
-      <div>
-        <h2>步驟</h2>
-        <ol class="wrap">
-          <li v-for="(step, index) in productList[0].step" :key="index">
-            <span class="number">{{ index + 1 }}</span>
-            <div class="title_border">
-              <p class="title">{{ step.title }}</p>
-            </div>
-            <ul>
-              <li v-for="(subStep, subIndex) in step.step" :key="subIndex">
-                <p class="info">{{ subStep }}</p>
-              </li>
-            </ul>
-          </li>
-        </ol>
-      </div>
-      <div>
-        <h2>過敏原</h2>
-        <div class="wrap">
-          <p class="allergy" v-for="(item, index) in productList[0].allergy" :key="index">{{ item }}</p>
-        </div>
-      </div>
-    </article>
-  </section>
-  <!-- <section>
-    <div class="pro_header">
-      <button class="btn_s btn_left">
-        <span>back</span>
-        <i class="fa-sharp fa-solid fa-arrow-left"></i>
-      </button>
-      <div class="title">
-        <div class="type">
-          <span>{{ newProduct.catagory }}</span>
-        </div>
-        {{ newProduct.name }}
-      </div>
-    </div>
-    <div class="pro_info">
-      <div class="pic">
-        <img :src="newProduct.img" alt="product" />
-      </div>
-      <div class="text">
-        <p>{{ productList[0].present }}</p>
-      </div>
-    </div>
-  </section> -->
-  <aside>
-    <div class="hot_ranking">
-      <div class="hot_title">
-        <img src="../assets/images/product/crown.svg" alt="" />
-        熱門推薦
-      </div>
-      <div class="hot_product" v-for="item in productList" :key="item.id" @click="update(item)">
+      <div class="pro_info">
         <div class="pic">
-          <img :src="item.img" alt="" />
+          <img :src="newProduct.img" alt="product">
         </div>
-        <div class="hot_info">
-          <div class="hot_type">{{ item.catagory }}</div>
-          <div class="hot_name">{{ item.name }}</div>
+        <div class="text">
+          <p>{{ newProduct.des }}</p>
         </div>
       </div>
-    </div>
-  </aside>
+      <article>
+        <div>
+          <h2>材料【2人份】</h2>
+          <div class="wrap">
+            <p class="ingred" v-for="(item, index) in newProduct.ingred" :key="index">{{ item }}</p>
+          </div>
+        </div>
+        <div>
+          <h2>步驟</h2>
+          <ol class="wrap">
+            <li v-for="(step, index) in newProduct.step" :key="index">
+              <span class="number">{{ index + 1 }}</span>
+              <div class="title_border">
+                <p class="title">{{ step.title }}</p>
+              </div>
+              <ul>
+                <li v-for="(subStep, subIndex) in step.step" :key="subIndex">
+                  <p class="info">{{ subStep }}</p>
+                </li>
+              </ul>
+            </li>
+          </ol>
+        </div>
+        <div>
+          <h2>過敏原</h2>
+          <div class="wrap">
+            <p class="allergy" v-for="(item, index) in newProduct.allergy" :key="index">{{ item }}</p>
+          </div>
+        </div>
+      </article>
+    </section>
+    <aside>
+      <div class="hot_ranking">
+        <div class="hot_title">
+          <img src="../assets/images/product/crown.svg" alt="" />
+          熱門推薦
+        </div>
+        <div class="hot_product" v-for="item in productList" :key="item.id" @click="update(item)">
+          <div class="pic">
+            <img :src="item.img" alt="" />
+          </div>
+          <div class="hot_info">
+            <div class="hot_type">{{ item.catagory }}</div>
+            <div class="hot_name">{{ item.name }}</div>
+          </div>
+        </div>
+      </div>
+    </aside>
+  </div>
 </template>
 
 <script>
@@ -97,11 +77,7 @@ import productList from "@/assets/data/productList.js";
 export default {
   data() {
     return {
-      newProduct: {
-        name: "泰式綠咖哩",
-        catagory: "主菜",
-        img: require("@/assets/images/product/10.jpg"),
-      },
+      newProduct: productList[0],
       productList,
     };
   },
