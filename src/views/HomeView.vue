69e2b05a2f97a1e2a9f74f2921ef1106d64f160b
dailinco <daylinc01229@gmail.com>
2023-07-11 20:57:39 +0800

    更新資訊

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  </div>
+lll
+<!-- <swiper
+    :effect="'cards'"
+    :grabCursor="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper> -->
 </template>
 
-<script>
+<!-- <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -15,4 +24,42 @@ export default {
   //   HelloWorld
   }
 }
+</script> -->
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+  //   HelloWorld
+  }
+}
 </script>
+<!-- <script>
+  // Import Swiper Vue.js components
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+
+  // Import Swiper styles
+  import 'swiper/css';
+
+  import 'swiper/css/effect-cards';
+
+  import './style.css';
+
+  // import required modules
+  import { EffectCards } from 'swiper/modules';
+
+  export default {
+    components: {
+      Swiper,
+      SwiperSlide,
+    },
+    setup() {
+      return {
+        modules: [EffectCards],
+      };
+    },
+  };
+</script> -->
