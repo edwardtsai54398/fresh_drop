627ecddb5dae0ed4ada455d2d1365c83e53dc476
edwardtsai54398 <fashion.donk@gmail.com>
2023-07-12 21:16:11 +0800

    header, footer 切好

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,30 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <MainHeader></MainHeader>
-  <nav>
-    <router-link to="/">fresh drop</router-link> |
-    <router-link to="/shop">SHOP</router-link> |
-    <router-link to="/login">Log in</router-link>|
-    <router-link to="/product">ProductDetail</router-link>
-  </nav>
+  <MainHeader @toggle="isModalOpen = !isModalOpen"></MainHeader>
+  <loginModal :isopen="isModalOpen" @close="isModalOpen = false"></loginModal>
+
   <main><router-view/></main>
-  <!-- <MainFooter></MainFooter>   -->
-  <!-- <HelloWorld></HelloWorld>    -->
-  <!-- 沒加上面這行npm run serve會報錯 GPT說是偵測引入了未使用的組件引起 -->
+
+  <MainFooter></MainFooter>  
 </template>
 <script>
-//有component在這裡 import
-import MainHeader from '@/components/MainHeader.vue'
+import MainHeader from '@/components/MainHeader.vue';
+import MainFooter from '@/components/MainFooter.vue';
+import loginModal from '@/components/loginModal.vue';
 
 export default {
   name: 'HomeView',
   components: {
     MainHeader,
-  }
+    MainFooter,
+    loginModal,
+  },
+  data() {
+    return {
+      isModalOpen: false,
+    };
+  },
 }
 </script>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -52,5 +52,5 @@ export default {
 </script>
 <style lang="scss">
 @import "@/assets/scss/all.scss";
-@import "@/assets/scss/page/login.scss";
+@import "@/assets/scss/layout/login.scss";
 </style>
