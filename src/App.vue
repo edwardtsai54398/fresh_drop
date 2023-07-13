0594d1470474179e2e63d0d23503994ecb281f22
bob821229 <blue821229@gmail.com>
2023-07-13 18:34:37 +0800

    新增9-12

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <template>
-<<<<<<< HEAD
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- <MainHeader></MainHeader>   -->
-  <nav>
-    <router-link to="/">fresh drop</router-link> |
-    <router-link to="/shop">SHOP</router-link> |
-    <router-link to="/login">Log in</router-link>|
-    <router-link to="/product">ProductDetail</router-link>|
-    <router-link to="/game">Game</router-link>|
-    <router-link to="/faq">FAQ</router-link>
-  </nav>
-  <main><router-view /></main>
-  <!-- <MainFooter></MainFooter>   -->
-  <!-- <HelloWorld></HelloWorld>    -->
-  <!-- 沒加上面這行npm run serve會報錯 GPT說是偵測引入了未使用的組件引起 -->
-=======
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
   <MainHeader @toggle="isModalOpen = !isModalOpen"></MainHeader>
   <loginModal :isopen="isModalOpen" @close="isModalOpen = false"></loginModal>
 
-  <main><router-view/></main>
+  <main><router-view /></main>
 
-  <MainFooter></MainFooter>  
->>>>>>> dev
+  <MainFooter></MainFooter>
 </template>
 <script>
 import MainHeader from '@/components/MainHeader.vue';
@@ -47,8 +28,3 @@ export default {
   },
 }
 </script>
-
-
-<style lang="scss">
-@import "@/assets/scss/all.scss";
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -63,22 +63,22 @@ const routesUser = [
     component: () => import(/* webpackChunkName: "about" */ '../views/MeatView.vue')
   },
   {
-<<<<<<< HEAD
     path: '/certified',
     name: 'certified',
-=======
-    path: '/faq',
-    name: 'faq',
->>>>>>> bob
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: AboutView,
-<<<<<<< HEAD
     component: () => import(/* webpackChunkName: "about" */ '../views/CertifiedView.vue')
-=======
+  },
+  {
+    path: '/faq',
+    name: 'faq',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: AboutView,
     component: () => import(/* webpackChunkName: "about" */ '../views/FaqView.vue')
->>>>>>> bob
   },
 ]
 
