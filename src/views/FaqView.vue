8a7a7fe561928e4c8cbc404d1a4a6e734656321d
bob821229 <blue821229@gmail.com>
2023-07-16 01:24:11 +0800

    faq可以切換了QQ

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -2,26 +2,28 @@
         this page is FAQ
         <div class="container">
                 <section class="question_type">
-                        <div class="type" v-for="(item, index) in faq" :key="index">
+                        <div class="type" v-for="(item, index) in faq" :key="index" @click="update(index)">
                                 <p>{{ item.type }}</p>
                         </div>
                 </section>
                 <p class="question_title">
-                        {{ faq[0].type }}
+                        {{ newFaq.type }}
                 </p>
-                <div class="group_wrap" v-for="(item, index) in newFaq.questions" :key="index">
+                <div class="group_wrap" v-for="(item, index) in newFaq.qa" :key="index" @click="toggleAnswer(item)">
                         <div class="icon">
                                 <svg width="12" height="18" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M12 9.18144L0.75 17.9988L0.75 0.364055L12 9.18144Z" fill="#1F8D61" />
                                 </svg>
                         </div>
-                        <div class="qa" v-for="(item, index) in newFaq.qa" :key="index">
+                        <div class="qa">
                                 <div class="question">
                                         {{ item.questions }}
                                 </div>
-                                <div class="answer">
-                                        {{ item.answers }}
-                                </div>
+                                <transition name="slide">
+                                        <div class="answer" v-if="item.open">
+                                                {{ item.answers }}
+                                        </div>
+                                </transition>
                         </div>
 
                 </div>
@@ -32,11 +34,22 @@ import faq from "@/assets/data/faq.js";
 export default {
         data() {
                 return {
-                        newFaq: faq[0],
+                        newFaq: faq[0],//預設顯示[0]的內容
                         faq,
                 }
         },
         methods: {
+                //切換下方問答群組
+                update(index) {
+                        this.newFaq = { ...this.faq[index] };//改變newFaq的資料
+                },
+                //qa開合用
+                toggleAnswer(item) {
+                        this.newFaq.qa.forEach((qa) => {
+                                qa.open = false; // 把所有的欄位都關閉
+                        });
+                        item.open = !item.open; // 切换布林值
+                }
         }
 }
 </script>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -27,11 +27,7 @@
                     <div>
                         <p>材料</p>
                         <div class="wrap">
-                            <p
-                                class="ingred"
-                                v-for="(item, index) in newProduct.ingred"
-                                :key="index"
-                            >
+                            <p class="ingred" v-for="(item, index) in newProduct.ingred" :key="index">
                                 {{ item }}
                             </p>
                         </div>
@@ -39,19 +35,13 @@
                     <div>
                         <p>步驟</p>
                         <ol class="wrap">
-                            <li
-                                v-for="(step, index) in newProduct.step"
-                                :key="index"
-                            >
+                            <li v-for="(step, index) in newProduct.step" :key="index">
                                 <span class="number">{{ index + 1 }}</span>
                                 <div class="title_border">
                                     <p class="title">{{ step.title }}</p>
                                 </div>
                                 <ul>
-                                    <li
-                                        v-for="(subStep, subIndex) in step.step"
-                                        :key="subIndex"
-                                    >
+                                    <li v-for="(subStep, subIndex) in step.step" :key="subIndex">
                                         <p class="info">{{ subStep }}</p>
                                     </li>
                                 </ul>
@@ -61,11 +51,7 @@
                     <div>
                         <p>過敏原</p>
                         <div class="wrap">
-                            <p
-                                class="allergy"
-                                v-for="(item, index) in newProduct.allergy"
-                                :key="index"
-                            >
+                            <p class="allergy" v-for="(item, index) in newProduct.allergy" :key="index">
                                 {{ item }}
                             </p>
                         </div>
@@ -76,18 +62,11 @@
             <aside class="recommend">
                 <div class="hot_ranking">
                     <div class="hot_title">
-                        <img
-                            src="../assets/images/product/crown.svg"
-                            alt="recommend pic"
-                        />
+                        <img src="../assets/images/product/crown.svg" alt="recommend pic" />
                         熱門推薦
                     </div>
-                    <div
-                        class="hot_product"
-                        v-for="(item, index) in filteredProductList"
-                        :key="item.id"
-                        @click="update(index)"
-                    >
+                    <div class="hot_product" v-for="(item, index) in filteredProductList" :key="item.id"
+                        @click="update(index)">
                         <div class="pic">
                             <img :src="item.img" alt="" />
                         </div>
@@ -106,11 +85,7 @@
                 <button class="more" @click="toggleExpend">看更多</button>
             </div>
             <div class="cook row">
-                <div
-                    class="wrap col-6 col-md-3"
-                    v-for="(item, index) in filteredProductShare"
-                    :key="index"
-                >
+                <div class="wrap col-6 col-md-3" v-for="(item, index) in filteredProductShare" :key="index">
                     <div class="card">
                         <div class="pic pic_food">
                             <img :src="item.img[0]" alt="share_food" />
@@ -118,10 +93,7 @@
                         <div class="user">
                             <div class="wrap">
                                 <div class="pic pic_people">
-                                    <img
-                                        :src="item.img[1]"
-                                        alt="share_people"
-                                    />
+                                    <img :src="item.img[1]" alt="share_people" />
                                 </div>
                                 <div class="name">
                                     {{ item.name }}
@@ -162,11 +134,7 @@
                         </button>
                     </div>
                     <div class="text_wrap">
-                        <textarea
-                            v-model="text"
-                            placeholder="說說你的烹飪心得吧！"
-                            class="upload_text"
-                        ></textarea>
+                        <textarea v-model="text" placeholder="說說你的烹飪心得吧！" class="upload_text"></textarea>
                     </div>
                 </div>
                 <button @click="uploadText" class="upload_btn btn_m">
@@ -187,21 +155,14 @@
             </div>
             <div class="content">
                 <div class="row">
-                    <div
-                        class="card col-6 col-md-3"
-                        v-for="(item, index) in productShare"
-                        :key="index"
-                    >
+                    <div class="card col-6 col-md-3" v-for="(item, index) in productShare" :key="index">
                         <div class="pic pic_food">
                             <img :src="item.img[0]" alt="share_food" />
                         </div>
                         <div class="user">
                             <div class="wrap">
                                 <div class="pic pic_people">
-                                    <img
-                                        :src="item.img[1]"
-                                        alt="share_people"
-                                    />
+                                    <img :src="item.img[1]" alt="share_people" />
                                 </div>
                                 <div class="name">
                                     {{ item.name }}
