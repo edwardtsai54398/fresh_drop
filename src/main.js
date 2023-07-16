56d94ec2c0eb46b35b62f8820a630ceebf3ed3e0
bob821229 <blue821229@gmail.com>
2023-07-16 17:00:25 +0800

    faq rwd & 安裝iviewui 套件

--- src/main.js
+++ src/main.js
@@ -4,3 +4,14 @@ import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+
+// view-ui-plus
+// import ViewUIPlus from 'view-ui-plus'
+// import 'view-ui-plus/dist/styles/viewuiplus.css'
+
+// const app = createApp(App)
+// app.use(store).use(router)
+// app.use(ViewUIPlus)
+// app.mount('#app')
+
+
