e9a7da80bf100971fa3a7877e3902d5d8cd10719
dailinco <daylinc01229@gmail.com>
2023-07-17 18:49:19 +0800

    解衝突

--- vue.config.js
+++ vue.config.js
@@ -11,14 +11,4 @@ module.exports = defineConfig({
       ]
     }
   }
-  transpileDependencies: true,
-
-  pluginOptions: {
-    'style-resources-loader': {
-      preProcessor: 'scss',
-      patterns: [
-        path.resolve(__dirname, './src/assets/scss/all.scss'),
-      ]
-    }
-  }
 })
