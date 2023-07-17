ab82681e7c92314e80d8285d06f13b275b049560
lannysyu <oopsjohn.77@gmail.com>
2023-07-17 17:21:13 +0800

    更新

--- src/views/FaqView.vue
+++ src/views/FaqView.vue
@@ -1,44 +1,55 @@
 <template>
-        this page is FAQ
-        <div class="container">
-                <section class="question_type">
-                        <div class="type" v-for="(item, index) in faq" :key="index">
-                                <p>{{ item.type }}</p>
-                        </div>
-                </section>
-                <p class="question_title">
-                        {{ faq[0].type }}
-                </p>
-                <div class="group_wrap" v-for="(item, index) in newFaq.questions" :key="index">
-                        <div class="icon">
-                                <svg width="12" height="18" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M12 9.18144L0.75 17.9988L0.75 0.364055L12 9.18144Z" fill="#1F8D61" />
-                                </svg>
-                        </div>
-                        <div class="qa" v-for="(item, index) in newFaq.qa" :key="index">
-                                <div class="question">
-                                        {{ item.questions }}
-                                </div>
-                                <div class="answer">
-                                        {{ item.answers }}
-                                </div>
-                        </div>
-
+    this page is FAQ
+    <div class="container">
+        <section class="question_type">
+            <div class="type" v-for="(item, index) in faq" :key="index">
+                <p>{{ item.type }}</p>
+            </div>
+        </section>
+        <p class="question_title">
+            {{ faq[0].type }}
+        </p>
+        <div
+            class="group_wrap"
+            v-for="(item, index) in newFaq.questions"
+            :key="index"
+        >
+            <div class="icon">
+                <svg
+                    width="12"
+                    height="18"
+                    viewBox="0 0 12 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M12 9.18144L0.75 17.9988L0.75 0.364055L12 9.18144Z"
+                        fill="#1F8D61"
+                    />
+                </svg>
+            </div>
+            <div class="qa" v-for="(item, index) in newFaq.qa" :key="index">
+                <div class="question">
+                    {{ item.questions }}
                 </div>
+                <div class="answer">
+                    {{ item.answers }}
+                </div>
+            </div>
         </div>
+    </div>
 </template>
 <script>
 import faq from "@/assets/data/faq.js";
 export default {
-        data() {
-                return {
-                        newFaq: faq[0],
-                        faq,
-                }
-        },
-        methods: {
-        }
-}
+    data() {
+        return {
+            newFaq: faq[0],
+            faq,
+        };
+    },
+    methods: {},
+};
 </script>
 <style scoped lang="scss">
 @import "@/assets/scss/all.scss";
--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,95 +1,105 @@
 <template>
-        <input type="text" class="search" placeholder="請輸入商品名稱" v-model="searchText" @input="searchFilter(searchText)">
-        <div class="container">
-                <h4 class="menu_title" v-show="mainDishFilter.length > 0">主菜 MAIN DISH</h4>
-                <div class="wrap_main_dish">
-                        <div class="card" v-for="(item, index) in mainDishFilter" :key="index">
-                                <div class="pic">
-                                        <img :src="item.img">
-                                </div>
-                                <h3>{{ item.name }}</h3>
-                                <div class="btn_scd_s">選購</div>
-                        </div>
+    <input
+        type="text"
+        class="search"
+        placeholder="請輸入商品名稱"
+        v-model="searchText"
+        @input="searchFilter(searchText)"
+    />
+    <div class="container">
+        <h4 class="menu_title" v-show="mainDishFilter.length > 0">
+            主菜 MAIN DISH
+        </h4>
+        <div class="wrap_main_dish">
+            <div
+                class="card"
+                v-for="(item, index) in mainDishFilter"
+                :key="index"
+            >
+                <div class="pic">
+                    <img :src="item.img" />
                 </div>
+                <h3>{{ item.name }}</h3>
+                <div class="btn_scd_s">選購</div>
+            </div>
         </div>
-        <div class="container">
-                <h4 class="menu_title" v-show="soupFilter.length > 0">湯品 SOUP</h4>
-                <div class="wrap_main_dish">
-                        <div class="card" v-for="(item, index) in soupFilter" :key="index">
-                                <div class="pic">
-                                        <img :src="item.img">
-                                </div>
-                                <h3>{{ item.name }}</h3>
-                                <div class="btn_scd_s">選購</div>
-                        </div>
+    </div>
+    <div class="container">
+        <h4 class="menu_title" v-show="soupFilter.length > 0">湯品 SOUP</h4>
+        <div class="wrap_main_dish">
+            <div class="card" v-for="(item, index) in soupFilter" :key="index">
+                <div class="pic">
+                    <img :src="item.img" />
                 </div>
+                <h3>{{ item.name }}</h3>
+                <div class="btn_scd_s">選購</div>
+            </div>
         </div>
-        <div class="container">
-                <h4 class="menu_title" v-show="saladFilter.length > 0">沙拉 SALAD</h4>
-                <div class="wrap_main_dish">
-                        <div class="card" v-for="(item, index) in saladFilter" :key="index">
-                                <div class="pic">
-                                        <img :src="item.img">
-                                </div>
-                                <h3>{{ item.name }}</h3>
-                                <div class="btn_scd_s">選購</div>
-                        </div>
+    </div>
+    <div class="container">
+        <h4 class="menu_title" v-show="saladFilter.length > 0">沙拉 SALAD</h4>
+        <div class="wrap_main_dish">
+            <div class="card" v-for="(item, index) in saladFilter" :key="index">
+                <div class="pic">
+                    <img :src="item.img" />
                 </div>
+                <h3>{{ item.name }}</h3>
+                <div class="btn_scd_s">選購</div>
+            </div>
         </div>
+    </div>
 </template>
 <script>
-import productList from '@/assets/data/productList.js'
+import productList from "@/assets/data/productList.js";
 export default {
-        data() {
-                return {
-                        productList,
-                        searchText: '',
-                        searchList: productList
+    data() {
+        return {
+            productList,
+            searchText: "",
+            searchList: productList,
+        };
+    },
+    computed: {
+        mainDishFilter() {
+            let filterResult = [];
+            this.searchList.forEach((item, index) => {
+                if (item.category == "主菜") {
+                    filterResult.push(this.searchList[index]);
                 }
+            });
+
+            return filterResult;
         },
-        computed: {
-                mainDishFilter() {
-                        let filterResult = []
-                        this.searchList.forEach((item, index) => {
-                                if (item.catagory == '主菜') {
-                                        filterResult.push(this.searchList[index])
-                                }
-                        })
-
-                        return filterResult
-                },
-                soupFilter() {
-                        let filterResult = []
-                        this.searchList.forEach((item, index) => {
-                                if (item.catagory == '湯品') {
-                                        filterResult.push(this.searchList[index])
-                                }
-                        })
-
-                        return filterResult
-                },
-                saladFilter() {
-                        let filterResult = []
-                        this.searchList.forEach((item, index) => {
-                                if (item.catagory == '沙拉') {
-                                        filterResult.push(this.searchList[index])
-                                }
-                        })
-
-                        return filterResult
-                },
-
-
-        },
-        methods: {
-                searchFilter(text) {
-                        let result = this.productList.filter((item) => {
-                                return item.name.includes(text)
-                        })
-                        this.searchList = result
+        soupFilter() {
+            let filterResult = [];
+            this.searchList.forEach((item, index) => {
+                if (item.category == "湯品") {
+                    filterResult.push(this.searchList[index]);
                 }
-        }
-}
+            });
+
+            return filterResult;
+        },
+        saladFilter() {
+            let filterResult = [];
+            this.searchList.forEach((item, index) => {
+                if (item.category == "沙拉") {
+                    filterResult.push(this.searchList[index]);
+                }
+            });
+
+            return filterResult;
+        },
+    },
+    methods: {
+        searchFilter(text) {
+            let result = this.productList.filter((item) => {
+                return item.name.includes(text);
+            });
+            this.searchList = result;
+        },
+    },
+};
 </script>
 <style lang="scss">
 @import "@/assets/scss/all.scss";
