e703a64869ffa7070f2ce10d80c356cb707d2800
edwardtsai54398 <fashion.donk@gmail.com>
2023-07-19 13:09:24 +0800

    新增ArrowBtn元件

---
+++ src/components/ArrowBtn.vue
@@ -0,0 +1,60 @@
+<template>
+    <button class="btn_arrow" href="" 
+    :class="{'btn_flat':isFlat, [`btn_${size}`]:true}"
+    @mouseover="hover" @mouseleave="mousLeave">
+        {{ text }}
+        <img src="@/assets/images/icon_bg/arrow_long_prim.svg" alt="" v-if="isBgcFFF">
+        <img src="@/assets/images/icon_bg/arrow_long_fff.svg" alt="" v-if="!isBgcFFF">
+    </button>
+</template>
+<script>
+
+    export default {
+      name: 'ArrowBtn',
+      props: {
+        size:{
+            size:String,
+            default: 's'
+        },
+        isFlat:{
+            isFlat:Boolean,
+            default: false,
+        },
+        text: String,
+      },
+      data() {
+        return {
+          isBgcFFF: this.isFlat,
+        };
+      },
+      computed:{
+
+      },
+      methods: {
+        hover() {
+            if(this.isFlat){
+                this.isBgcFFF = false
+            }
+        },
+        mousLeave() {
+            if(this.isFlat){
+                this.isBgcFFF = true
+            }
+        }
+      },
+    }
+</script>
+<style lang="scss">
+    @import "@/assets/scss/all.scss";
+    .btn_arrow{
+        display: flex;
+        align-items: center;
+        img[src*="arrow_long"]{
+            margin-left: $sp2;
+        }
+        &.btn_xs img{
+            margin-left: $sp1;
+            width: 36px;
+        }
+    }
+</style>
