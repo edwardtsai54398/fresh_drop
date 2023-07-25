5a7631fa91aa2ff285a29ed389eb8434f91df780
edwardtsai54398 <fashion.donk@gmail.com>
2023-07-25 15:38:49 +0800

    禮物卡到結帳頁vuex完成

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,11 @@ export default createStore({
     state: {
         isLogin: false,
         memberInfoAll: {},
-        giftBuy: {},
+        giftBuy: {
+            name: '',
+            img: '',
+            money: 0
+        },
         cartList: [[]],
         shopPlan: {
             plan: '',
@@ -27,6 +31,21 @@ export default createStore({
             state.shopPlan.plan = planWrap.plan
             state.shopPlan.meal = planWrap.meal
             state.shopPlan.week = planWrap.week
+        },
+        stateGiftBuy(state, giftWrap) {
+            state.giftBuy.name = giftWrap.name
+            state.giftBuy.img = giftWrap.img
+            state.giftBuy.money = giftWrap.money
+        },
+        clearState(state, string) {
+            if (string == 'cart') {
+                state.cartList = [[]]
+                state.shopPlan.plan = ''
+                state.shopPlan.meal = 0
+                state.shopPlan.week = 1
+            } else if (string == 'gift') {
+                state.giftBuy = {}
+            }
         }
     },
     actions: {
