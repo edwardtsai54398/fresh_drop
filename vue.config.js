ebaeb00d55e79b662ee7f5fd1e9d846e0ae61f6c
huang-li-ping <happyporkrice@gmail.com>
2023-07-26 11:15:04 +0800

    首頁小遊戲fixed element & 加網頁頁籤

--- vue.config.js
+++ vue.config.js
@@ -10,8 +10,19 @@ module.exports = defineConfig({
         path.resolve(__dirname, './src/assets/scss/all.scss'),
       ]
     }
+  },
+  pwa:{
+    iconPaths:{
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+
+    }
   }
-})
+},
+)
 
 // module.exports = {
 //     publicPath: process.env.NODE_ENV === 'production'
