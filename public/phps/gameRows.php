158906df69f52574578b5f0166a1da35fa4300d0
bob821229 <blue821229@gmail.com>
2023-08-16 16:29:51 +0800

    修正 測驗結果丟進購物車 game faq狀態:未套用 不撈資料到前台

--- public/phps/gameRows.php
+++ public/phps/gameRows.php
@@ -13,7 +13,8 @@ try {
     (SELECT recipe_pic FROM recipe WHERE recipe_no = g.dish2) AS dish2_pic,
     (SELECT recipe_pic FROM recipe WHERE recipe_no = g.dish3) AS dish3_pic,
     (SELECT recipe_pic FROM recipe WHERE recipe_no = g.dish4) AS dish4_pic
-FROM game AS g;";
+FROM game AS g 
+WHERE g.status = 1";
     $game = $pdo->query($sql);
     $gameRows = $game->fetchAll(PDO::FETCH_ASSOC);
     // echo json_encode(["連線成功"]);
